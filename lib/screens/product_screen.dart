6036b7dbb6d65f2542c6080b344e5a0fd877a4b5
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-04-11 23:32:01 +0700

    product screen: and product and delivery information and bottom app bar

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -22,7 +22,40 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(title: product.name),
-      bottomNavigationBar: const CustomNavBar(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: SizedBox(
+          height: 70,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.share,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.white,
+                ),
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'ADD TO CART',
+                    style: Theme.of(context).textTheme.displaySmall,
+                  )),
+            ],
+          ),
+        ),
+      ),
       body: ListView(
         children: [
           CarouselSlider(
@@ -74,6 +107,41 @@ class ProductScreen extends StatelessWidget {
                             .copyWith(color: Colors.white),
                       ),
                     ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ExpansionTile(
+              initiallyExpanded: true,
+              title: Text(
+                'Product Information',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              children: [
+                ListTile(
+                  title: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare id nibh at venenatis. Pellentesque volutpat nulla sed mauris venenatis varius eu id risus.',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: ExpansionTile(
+              title: Text(
+                'Delivery Information',
+                style: Theme.of(context).textTheme.displaySmall,
+              ),
+              children: [
+                ListTile(
+                  title: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare id nibh at venenatis. Pellentesque volutpat nulla sed mauris venenatis varius eu id risus.',
+                    style: Theme.of(context).textTheme.bodyLarge,
                   ),
                 ),
               ],
