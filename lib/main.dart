476d9bfbe82db9e0271000fddeed9c93be40ad84
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-04-12 13:31:31 +0700

    add wishlist screen and bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
 import 'package:flutter_ecommerce_app/config/app_router.dart';
 import 'package:flutter_ecommerce_app/config/theme.dart';
 
@@ -16,11 +18,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: theme(),
-      home: const HomeScreen(),
-      onGenerateRoute: appRouter.onGenerateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => WishlistBloc()..add(StartWishlist()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: theme(),
+        home: const HomeScreen(),
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
     );
   }
 }
--- lib/screens/wishlist_screen.dart
+++ lib/screens/wishlist_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
 
 import '../models/models.dart';
 import '../widgets/widgets.dart';
@@ -19,23 +21,37 @@ class WishlistScreen extends StatelessWidget {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Wishlist'),
       bottomNavigationBar: const CustomNavBar(),
-      body: GridView.builder(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 8,
-          vertical: 16,
-        ),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-          childAspectRatio: 2.4,
-        ),
-        itemCount: Product.products.length,
-        itemBuilder: (context, index) => Center(
-            child: ProductCard(
-          product: Product.products[index],
-          widthFactor: 1.1,
-          leftPosition: 100,
-          isWishlist: true,
-        )),
+      body: BlocBuilder<WishlistBloc, WishlistState>(
+        builder: (context, state) {
+          // Display base on event state.
+          if (state is WishlistLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is WishlistLoaded) {
+            return GridView.builder(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 16,
+              ),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                childAspectRatio: 2.4,
+              ),
+              itemCount: state.wishlist.products.length,
+              itemBuilder: (context, index) => Center(
+                  child: ProductCard(
+                product: state.wishlist.products[index],
+                widthFactor: 1.1,
+                leftPosition: 100,
+                isWishlist: true,
+              )),
+            );
+          } else {
+            return const Text('Something went wrong');
+          }
+        },
       ),
     );
   }
