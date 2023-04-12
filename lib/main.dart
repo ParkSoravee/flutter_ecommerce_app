29ab47d68b5842c331fe373db5d0cbcc6b10c947
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-04-12 21:10:56 +0700

    set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/bloc/cart/cart_bloc.dart';
@@ -7,7 +8,11 @@ import 'package:flutter_ecommerce_app/config/theme.dart';
 
 import 'screens/screens.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
   runApp(MyApp(
     appRouter: AppRouter(),
   ));
