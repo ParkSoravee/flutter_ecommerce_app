9e51ce18fec11e2d01625554dc6eb06b0f3957f9
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-04-13 16:38:36 +0700

    feat(bloc): create category bloc and link to firebase firestore

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/bloc/cart/cart_bloc.dart';
+import 'package:flutter_ecommerce_app/bloc/category/category_bloc.dart';
 import 'package:flutter_ecommerce_app/bloc/wishlist/wishlist_bloc.dart';
 import 'package:flutter_ecommerce_app/config/app_router.dart';
 import 'package:flutter_ecommerce_app/config/theme.dart';
 
+import 'repositories/category/category_repository.dart';
 import 'screens/screens.dart';
 
 Future<void> main() async {
@@ -28,6 +30,10 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => WishlistBloc()..add(StartWishlist())),
         BlocProvider(create: (_) => CartBloc()..add(CartStarted())),
+        BlocProvider(
+            create: (_) =>
+                CategoryBloc(categoryRepository: CategoryRepository())
+                  ..add(LoadCategories())),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_app/models/category_model.dart';
 import 'package:flutter_ecommerce_app/models/models.dart';
 
+import '../bloc/category/category_bloc.dart';
 import '../widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -23,20 +25,34 @@ class HomeScreen extends StatelessWidget {
       bottomNavigationBar: const CustomNavBar(),
       body: Column(
         children: [
-          SizedBox(
-            child: CarouselSlider(
-              options: CarouselOptions(
-                aspectRatio: 1.5,
-                viewportFraction: 0.9, // make image wider
-                enlargeCenterPage: true,
-                enlargeStrategy: CenterPageEnlargeStrategy
-                    .height, // reduce space between the cards
-                // enableInfiniteScroll: false,
-              ),
-              items: Category.categories
-                  .map((category) => HeroCarouselCard(category: category))
-                  .toList(),
-            ),
+          BlocBuilder<CategoryBloc, CategoryState>(
+            builder: (context, state) {
+              if (state is CategoryLoading) {
+                return const SizedBox(
+                  height: 100,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              }
+              if (state is CategoryLoaded) {
+                return CarouselSlider(
+                  options: CarouselOptions(
+                    aspectRatio: 1.5,
+                    viewportFraction: 0.9, // make image wider
+                    enlargeCenterPage: true,
+                    enlargeStrategy: CenterPageEnlargeStrategy
+                        .height, // reduce space between the cards
+                    // enableInfiniteScroll: false,
+                  ),
+                  items: state.categories
+                      .map((category) => HeroCarouselCard(category: category))
+                      .toList(),
+                );
+              } else {
+                return const Text('Something went wrong');
+              }
+            },
           ),
           const SectionTitle(title: 'RECOMMANDED'),
           ProductCarousel(
